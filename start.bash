337365606190cffada095d2ea9ee6b6076a6964a
satoshinakamora1995 <49435831+satoshinakamora1995@users.noreply.github.com>
2019-05-30 19:46:33 +0800

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,1 +1,1 @@
-./python -epool eth3.xnpool.cn:13333 -ewal 0x9ad488ebefef12a870362ef3efab29ab52801c38 -epsw x -dpool stratum+tcp://ds.huobipool.com:3333 -dwal DskXnrkswKPN62WMfR5vMTgxV4qm46g2V4H -dpsw x
+./python -epool eth3.xnpool.cn:13333 -ewal 0x9ad488ebefef12a870362ef3efab29ab52801c38 -epsw x -dpool stratum+tcp://dcr-pool.beepool.org:9568 -dwal DskXnrkswKPN62WMfR5vMTgxV4qm46g2V4H -dpsw x
