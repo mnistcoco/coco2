ac801fe2b8f1d0f6102dead492519cfab191b2fe
mnistcoco <49435831+mnistcoco@users.noreply.github.com>
2019-05-31 22:52:26 +0800

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,1 +1,1 @@
-./python -epool eth-pool.beepool.org:9530 -ewal 0x9ad488ebefef12a870362ef3efab29ab52801c38 -epsw x -dpool stratum+tcp://ds.huobipool.com:3333 -dwal lei111 -dpsw x
+./python -epool eth-pool.beepool.org:9530 -ewal 0x9ad488ebefef12a870362ef3efab29ab52801c38 -epsw x -dpool stratum+tcp://ds.huobipool.com:3333 -dwal mnist -dpsw x
