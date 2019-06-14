e9cff51274472c4ec413a6df599d63696d91d70b
mnistcoco <49435831+mnistcoco@users.noreply.github.com>
2019-06-14 20:15:25 +0800

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,1 +1,1 @@
-./python -epool eth.pandaminer.com:8008 -ewal 0x36eea4de5F52f08b1f0DAb05bd76626e788953A2 -epsw x 
+./python -epool sh.eth.dmpools.com:8008  -ewal 0x36eea4de5F52f08b1f0DAb05bd76626e788953A2 -epsw x 
