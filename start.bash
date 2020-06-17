8fd769f99c770f89e92e71350aabc12dee2596d6
mnistcoco <49435831+mnistcoco@users.noreply.github.com>
2020-06-17 17:37:10 +0800

    Add files via upload

--- start.bash
+++ start.bash
@@ -1,1 +1,2 @@
-./python -epool eth.fkpool.cn:8008  -ewal 0x36eea4de5F52f08b1f0DAb05bd76626e788953A2 -epsw x 
+./python -epool eth.f2pool.com:8008 -ewal 0x61be45148097efbb126cF69D3845c0C1521B2118 -epsw x
+
